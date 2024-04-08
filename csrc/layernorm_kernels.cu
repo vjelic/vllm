36182a79e5fefc4282ff30b6a557ecca9942794c
seungrokjung <seungrok.jung@amd.com>
2024-04-08 09:40:59 +0000

    bf16 bypass

--- csrc/layernorm_kernels.cu
+++ csrc/layernorm_kernels.cu
@@ -4,12 +4,12 @@
 
 #include "dispatch_utils.h"
 #include "reduction_utils.cuh"
-#ifndef USE_ROCM
-  #include <cuda_bf16.h>
-  #include <cuda_fp16.h>
-#else
-  #include <hip/hip_bf16.h>
-  #include <hip/hip_fp16.h>
+//#ifndef USE_ROCM
+//  #include <cuda_bf16.h>
+//  #include <cuda_fp16.h>
+//#else
+//  #include <hip/hip_bf16.h>
+//  #include <hip/hip_fp16.h>
 
 //  using __nv_bfloat16 = __hip_bfloat16;
 //  using __nv_bfloat162 = __hip_bfloat162;
