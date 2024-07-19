3ad79624181db91abb4c53c704358ca200063206
Zakor Gyula <126694206+gyulaz-htec@users.noreply.github.com>
2024-07-19 18:31:51 +0200

    New LLM for MLPerf Server scenario serving (#93)
    Co-authored-by: Zakor <gyuzakor@amd.com>

--- vllm/__init__.py
+++ vllm/__init__.py
@@ -4,6 +4,7 @@ from vllm.engine.arg_utils import AsyncEngineArgs, EngineArgs
 from vllm.engine.async_llm_engine import AsyncLLMEngine
 from vllm.engine.llm_engine import LLMEngine
 from vllm.entrypoints.llm import LLM
+from vllm.entrypoints.queue_llm import QueueLLM
 from vllm.executor.ray_utils import initialize_ray_cluster
 from vllm.inputs import PromptStrictInputs, TextPrompt, TokensPrompt
 from vllm.model_executor.models import ModelRegistry
@@ -15,6 +16,7 @@ from vllm.sampling_params import SamplingParams
 __version__ = "0.4.3"
 
 __all__ = [
+    "QueueLLM"
     "LLM",
     "ModelRegistry",
     "PromptStrictInputs",
