dab00e865933a58a555b906a9fc48f89619d143b
Vinayak Gokhale <Vinayak.Gokhale@amd.com>
2024-07-30 18:18:04 +0000

    Revert broken load

--- vllm/model_executor/layers/fused_moe/fused_moe.py
+++ vllm/model_executor/layers/fused_moe/fused_moe.py
@@ -102,6 +102,7 @@ def fused_moe_kernel(
     # and accumulate
     # `a_ptrs` is a block of [BLOCK_SIZE_M, BLOCK_SIZE_K] pointers
     # `b_ptrs` is a block of [BLOCK_SIZE_K, BLOCK_SIZE_N] pointers
+    num_tokens_post_padded = tl.load(num_tokens_post_padded_ptr)
     if pid_m * BLOCK_SIZE_M >= num_tokens_post_padded:
         return
     offs_token_id = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
@@ -264,7 +265,7 @@ def invoke_fused_moe_kernel(A: torch.Tensor, B: torch.Tensor, C: torch.Tensor,
         topk_weights,
         sorted_token_ids,
         expert_ids,
-        num_tokens_post_padded[0].item(),
+        num_tokens_post_padded,
         B.shape[1],
         B.shape[2] - padding_size,
         sorted_token_ids.shape[0],
