9c91d54e4dd2ee6863d3aedc5c61caa47dd24bbf
Vinayak Gokhale <Vinayak.Gokhale@amd.com>
2024-08-05 13:36:46 +0000

    Use persistent kernel in microbenchmark

--- benchmarks/kernels/benchmark_mixtral_moe_rocm.py
+++ benchmarks/kernels/benchmark_mixtral_moe_rocm.py
@@ -49,18 +49,18 @@ def main(args):
 def get_full_tuning_space():
     configs = []
 
-    block_mn_range = [16, 32, 64, 128, 256]
-    block_k_range = [16, 32, 64, 128, 256]
+    block_mn_range = [32, 64, 128, 256]
+    block_k_range = [32, 64, 128, 256]
     # split_k_range = [1] #, 2, 4, 5, 6, 8, 10, 12, 16, 18, 24]
-    num_warps_range = [1, 2, 4, 8]
+    num_warps_range = [4, 8]
     group_m_range = [1, 4, 8, 16, 32]
     # For now we see better perf with num_stages=0 for all gemm configs we care
     # But keep this explicit so that we do not forget we may need to set it to
     # other values in the future
     num_stage_range = [0]
     waves_per_eu_range = [0]
-    matrix_instr_nonkdim_range = [16, 32]
-    kpack_range = [1, 2]
+    matrix_instr_nonkdim_range = [16]
+    kpack_range = [2]
 
     for block_m in block_mn_range:
         for block_n in block_mn_range:
@@ -377,7 +377,7 @@ def run_timing(
 
     start_event.record()
     for i in range(num_calls):
-        invoke_fused_moe_kernel(
+        invoke_fused_moe_persistent_kernel(
             hidden_states,
             w1,
             intermediate_cache1,
@@ -398,7 +398,7 @@ def run_timing(
 
         ops.silu_and_mul(intermediate_cache2, intermediate_cache1.view(-1, N))
 
-        invoke_fused_moe_kernel(
+        invoke_fused_moe_persistent_kernel(
             intermediate_cache2,
             w2,
             intermediate_cache3,
