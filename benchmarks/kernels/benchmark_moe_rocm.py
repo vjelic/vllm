d637b51b22811e70d0dd6b7c71f04a7d4c6c38f6
wunhuang <wunhuang@amd.com>
2024-08-16 04:40:41 +0000

    Change padding size to 256 for fp8

--- benchmarks/kernels/benchmark_moe_rocm.py
+++ benchmarks/kernels/benchmark_moe_rocm.py
@@ -13,7 +13,7 @@ from vllm.model_executor.layers.fused_moe import (fused_moe,
 
 from vllm import envs
 
-padding_size = 128 if envs.VLLM_MOE_PADDING else 0
+padding_size = 256 if envs.VLLM_MOE_PADDING else 0
 
 def permute_weight(x: torch.Tensor) -> torch.Tensor:
     ## Hardcode BLOCK_K and BLOCK_N
