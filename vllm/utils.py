52a22f4f3c383a8720b0dd9fb411f7ccf1171f42
Lzy17 <joeylzy0710@gmail.com>
2025-01-13 15:32:55 +0000

    Enable user marker for profiling

--- vllm/utils.py
+++ vllm/utils.py
@@ -282,6 +282,22 @@ class rpd_mark:
             return func
 
 
+class rpd_user_marker:
+    def __init__(self, name=None):
+        self.name = name
+        self.marker = None
+    def __enter__(self):
+        if is_hipScopedMarker_available():
+            from hipScopedMarker import hipScopedMarker
+            marker_name = self.name if self.name else "UserMarker Undefined"
+            self.marker = hipScopedMarker(f"{marker_name}")
+            self.marker.__enter__()
+        return self
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.marker:
+            self.marker.__exit__(exc_type, exc_val, exc_tb)
+
+
 class Device(enum.Enum):
     GPU = enum.auto()
     CPU = enum.auto()
