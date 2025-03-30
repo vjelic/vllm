a2435c6fc172bea8cc457e6ee86fd48d2c76704b
qli88 <qiang.li2@amd.com>
2025-03-30 06:02:35 +0000

    AITER MLA added

--- vllm/platforms/rocm.py
+++ vllm/platforms/rocm.py
@@ -13,6 +13,7 @@ import vllm.envs as envs
 from vllm.logger import init_logger
 
 from .interface import DeviceCapability, Platform, PlatformEnum, _Backend
+from vllm.utils import aiter_mla_enabled
 
 if TYPE_CHECKING:
     from vllm.config import VllmConfig
@@ -118,8 +119,12 @@ class RocmPlatform(Platform):
                              kv_cache_dtype, block_size, use_v1,
                              use_mla) -> str:
         if use_mla:
-            logger.info("Using Triton MLA backend.")
-            return "vllm.attention.backends.triton_mla.TritonMLABackend"
+            if aiter_mla_enabled():
+                logger.info("Using AITER MLA backend.")
+                return "vllm.attention.backends.rocm_aiter_mla.AiterMLABackend"
+            else:
+                logger.info("Using Triton MLA backend.")
+                return "vllm.attention.backends.triton_mla.TritonMLABackend"
         selected_backend = (_Backend.ROCM_FLASH if selected_backend
                             == _Backend.FLASH_ATTN else selected_backend)
         if envs.VLLM_USE_V1:
