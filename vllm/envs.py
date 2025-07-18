9be055fde2d014201d72a71732c31ab240efdfb8
tjtanaavllm <tunjian.tan@amd.com>
2025-07-18 15:48:09 +0000

    add AITER ROPE
    Signed-off-by: tjtanaavllm <tunjian.tan@amd.com>

--- vllm/envs.py
+++ vllm/envs.py
@@ -80,6 +80,7 @@ if TYPE_CHECKING:
     VLLM_ROCM_USE_AITER_ASMMOE: bool = False
     VLLM_ROCM_USE_AITER_RMSNORM: bool = True
     VLLM_ROCM_USE_AITER_MLA: bool = True
+    VLLM_ROCM_USE_AITER_ROPE: bool = True
     VLLM_ROCM_USE_SKINNY_GEMM: bool = True
     VLLM_ROCM_FP8_PADDING: bool = True
     VLLM_ROCM_MOE_PADDING: bool = True
@@ -565,6 +566,12 @@ environment_variables: dict[str, Callable[[], Any]] = {
     lambda: (os.getenv("VLLM_ROCM_USE_AITER_MLA", "True").lower() in
              ("true", "1")),
 
+    # Whether to use aiter rope op if aiter ops are enabled.
+    # By default is enabled.
+    "VLLM_ROCM_USE_AITER_ROPE":
+    lambda: (os.getenv("VLLM_ROCM_USE_AITER_ROPE", "True").lower() in
+             ("true", "1")),
+             
     # Use skinny gemm for FP8 kernels
     "VLLM_ROCM_USE_SKINNY_GEMM":
     lambda: (os.getenv("VLLM_ROCM_USE_SKINNY_GEMM", "True").lower() in
