d4fbc2e8c45c015b5a5d5b4e6d0e4d82b266ad68
tjtanaavllm <tunjian.tan@amd.com>
2025-07-24 15:39:42 +0800

    [FEAT] Add sharedexperts fusion aiter (#601)
    * add support for ptpc bpreshuffle gemm
    
    Signed-off-by: tjtanaavllm <tunjian.tan@amd.com>
    
    * fix asm moe for ptpc
    
    Signed-off-by: tjtanaavllm <tunjian.tan@amd.com>
    
    * enable ptpc fused moe for compressed tensors
    
    Signed-off-by: tjtanaavllm <tunjian.tan@amd.com>
    
    * upgrade aiter to aiter916bf3c; fix api bug
    
    Signed-off-by: tjtanaavllm <tunjian.tan@amd.com>
    
    * fix gemm_a8w8_bpreshuffle
    
    Signed-off-by: tjtanaavllm <tunjian.tan@amd.com>
    
    * add preliminary fused shared routed experts changes
    
    Signed-off-by: tjtanaavllm <tunjian.tan@amd.com>
    
    * add hip grouped topk and hip biased grouped topk from AITER
    
    Signed-off-by: tjtanaavllm <tunjian.tan@amd.com>
    
    ---------
    
    Signed-off-by: tjtanaavllm <tunjian.tan@amd.com>

--- vllm/model_executor/layers/quantization/utils/fp8_utils.py
+++ vllm/model_executor/layers/quantization/utils/fp8_utils.py
@@ -118,12 +118,12 @@ def apply_w8a8_block_fp8_linear(
 ) -> torch.Tensor:
     assert input_scale is None
     # View input as 2D matrix for fp8 methods
-    # print(f'input.shape: {input.shape}, input.stride: {input.stride()}')
     input_2d = input.view(-1, input.shape[-1])
     output_shape = [*input.shape[:-1], weight.shape[0]]
 
     shape_supported_by_cutlass = (weight.shape[0] % 128 == 0
-                                  and weight.shape[1] % 128 == 0) and current_platform.is_cuda()
+                                  and weight.shape[1] % 128
+                                  == 0) and current_platform.is_cuda()
 
     if cutlass_block_fp8_supported and shape_supported_by_cutlass:
         q_input, x_scale = per_token_group_quant_fp8(input_2d,
@@ -136,12 +136,11 @@ def apply_w8a8_block_fp8_linear(
                                        scale_b=weight_scale.T)
     else:
         if use_aiter_and_is_supported:
-            # print(f"input_2d.shape: {input_2d.shape}, input_2d.stride: {input_2d.stride()}")
-            q_input, x_scale = aiter_per1x128_quant(input_2d.contiguous(), quant_dtype=rocm_aiter.dtypes.fp8)
+            q_input, x_scale = aiter_per1x128_quant(
+                input_2d.contiguous(), quant_dtype=rocm_aiter.dtypes.fp8)
         else:
-            q_input, x_scale = per_token_group_quant_fp8(input_2d,
-                                                     block_size[1],
-                                                     column_major_scales=False)
+            q_input, x_scale = per_token_group_quant_fp8(
+                input_2d, block_size[1], column_major_scales=False)
         w8a8_blockscale_func = dispatch_w8a8_blockscale_func(
             False, use_aiter_and_is_supported)
 
