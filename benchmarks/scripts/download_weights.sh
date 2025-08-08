bf3cd2fea4a2a3459db753e51acd173504b40b8f
wuhuikx <hattie.wu@amd.com>
2025-08-08 12:52:54 +0800

    Add download weights scripts

---
+++ benchmarks/scripts/download_weights.sh
@@ -0,0 +1,11 @@
+export HF_HUB_ENABLE_HF_TRANSFER=1
+
+
+hf download amd/Llama-3.3-70B-Instruct-FP8-KV --local-dir ./Llama-3.3-70B-Instruct-FP8-KV
+#hf download amd/Llama-3.1-8B-Instruct-FP8-KV --local-dir ./amd/Llama-3.1-8B-Instruct-FP8-KV
+#hf download amd/Llama-3.3-70B-Instruct-MXFP4-Preview --local-dir ./amd/Llama-3.3-70B-Instruct-MXFP4-Preview
+
+
+#hf download Qwen/Qwen3-32B --local-dir Qwen/Qwen3-32B
+#hf download Qwen/Qwen3-32B-FP8 --local-dir Qwen/Qwen3-32B-FP8
+
