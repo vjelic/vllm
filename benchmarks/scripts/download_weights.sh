547274fd7485612437a3fa26df6744fc45559b96
zejunchen-zejun <zejun.chen@amd.com>
2025-08-13 19:00:26 +0800

    add the download command for 405B fp4/fp8
    Signed-off-by: zejunchen-zejun <zejun.chen@amd.com>

--- benchmarks/scripts/download_weights.sh
+++ benchmarks/scripts/download_weights.sh
@@ -4,13 +4,19 @@ export PATH="$HOME/.local/bin:$PATH"
 export HF_HUB_ENABLE_HF_TRANSFER=1
 export HF_HUB_OFFLINE=0
 
-
-huggingface-cli download amd/Llama-3.1-8B-Instruct-FP8-KV --local-dir ./amd/Llama-3.1-8B-Instruct-FP8-KV
+# llama - bf16
 huggingface-cli download meta-llama/Llama-3.3-70B-Instruct --local-dir ./meta-llama/Llama-3.3-70B-Instruct
+
+# llama - fp8
+huggingface-cli download amd/Llama-3.1-8B-Instruct-FP8-KV --local-dir ./amd/Llama-3.1-8B-Instruct-FP8-KV
 huggingface-cli download amd/Llama-3.3-70B-Instruct-FP8-KV --local-dir ./amd/Llama-3.3-70B-Instruct-FP8-KV
+huggingface-cli download amd/Llama-3.1-405B-Instruct-FP8-KV --local-dir ./amd/Llama-3.1-405B-Instruct-FP8-KV
+
+# llama fp4
 huggingface-cli download amd/Llama-3.3-70B-Instruct-MXFP4-Preview --local-dir ./amd/Llama-3.3-70B-Instruct-MXFP4-Preview
+huggingface-cli download amd/Llama-3.1-405B-Instruct-MXFP4-Preview --local-dir ./amd/Llama-3.1-405B-Instruct-MXFP4-Preview
 
-
+# qwen
 huggingface-cli download Qwen/Qwen3-32B --local-dir Qwen/Qwen3-32B
 huggingface-cli download Qwen/Qwen3-32B-FP8 --local-dir Qwen/Qwen3-32B-FP8
 huggingface-cli download Qwen/Qwen3-235B-A22B-FP8 --local-dir Qwen/Qwen3-235B-A22B-FP8
