577afcd7e891032574eb2c1fe9bd880f233fb394
wuhuikx <hattie.wu@amd.com>
2025-08-13 19:39:46 +0800

    Fix bug in running script

--- benchmarks/scripts/dense/multiple_model_benchmark.sh
+++ benchmarks/scripts/dense/multiple_model_benchmark.sh
@@ -5,12 +5,12 @@ array_out=(1024)
 #llama3.3 8B FP8 TP1
 MODEL=/home/zejchen/models/amd/Llama-3.1-8B-Instruct-FP8-KV
 TP=1
-Dtype=bfloat16
+DTYPE=bfloat16
 LOG_NAME=llama3.1-8B
 for bs in ${array_bs[@]}; do
     for IN in ${array_in[@]}; do
         for OUT in ${array_out[@]}; do
-            bash benchmark_throughput.sh $MODEL $TP $Dtype $bs $IN $OUT $LOG_NAME
+            bash benchmark_throughput.sh $MODEL $TP $DTYPE $bs $IN $OUT $LOG_NAME
         done
     done
 done
@@ -19,12 +19,12 @@ done
 #llama3.3 70B BF16 TP8
 MODEL=/home/zejchen/models/meta-llama/Llama-3.3-70B-Instruct
 TP=8
-Dtype=bfloat16
+DTYPE=bfloat16
 LOG_NAME=llama3.3-70B
 for bs in ${array_bs[@]}; do
     for IN in ${array_in[@]}; do
         for OUT in ${array_out[@]}; do
-            bash benchmark_throughput.sh $MODEL $TP $Dtype $bs $IN $OUT $LOG_NAME
+            bash benchmark_throughput.sh $MODEL $TP $DTYPE $bs $IN $OUT $LOG_NAME
         done
     done
 done
@@ -32,12 +32,12 @@ done
 #llama3.3 70B FP8 TP8
 MODEL=/home/zejchen/models/amd/Llama-3.3-70B-Instruct-FP8-KV/
 TP=8
-Dtype=bfloat16
+DTYPE=bfloat16
 LOG_NAME=llama3.3-70B
 for bs in ${array_bs[@]}; do
     for IN in ${array_in[@]}; do
         for OUT in ${array_out[@]}; do
-            bash benchmark_throughput.sh $MODEL $TP $Dtype $bs $IN $OUT $LOG_NAME
+            bash benchmark_throughput.sh $MODEL $TP $DTYPE $bs $IN $OUT $LOG_NAME
         done
     done
 done
@@ -50,7 +50,7 @@ LOG_NAME=Qwen3-32B
 for bs in ${array_bs[@]}; do
     for IN in ${array_in[@]}; do
         for OUT in ${array_out[@]}; do
-            bash benchmark_throughput.sh $MODEL $TP $Dtype $bs $IN $OUT $LOG_NAME
+            bash benchmark_throughput.sh $MODEL $TP $DTYPE $bs $IN $OUT $LOG_NAME
         done
     done
 done
@@ -63,7 +63,7 @@ LOG_NAME=Qwen3-32B-FP8
 for bs in ${array_bs[@]}; do
     for IN in ${array_in[@]}; do
         for OUT in ${array_out[@]}; do
-            bash benchmark_throughput.sh $MODEL $TP $Dtype $bs $IN $OUT $LOG_NAME
+            bash benchmark_throughput.sh $MODEL $TP $DTYPE $bs $IN $OUT $LOG_NAME
         done
     done
 done
@@ -76,7 +76,7 @@ LOG_NAME=Qwen3-32B-FP8
 for bs in ${array_bs[@]}; do
     for IN in ${array_in[@]}; do
         for OUT in ${array_out[@]}; do
-            bash benchmark_throughput.sh $MODEL $TP $Dtype $bs $IN $OUT $LOG_NAME
+            bash benchmark_throughput.sh $MODEL $TP $DTYPE $bs $IN $OUT $LOG_NAME
         done
     done
 done
