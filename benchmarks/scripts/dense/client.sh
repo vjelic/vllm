11158d17e0c978e172dceb3e573f31a9e034b781
wuhuikx <hattie.wu@amd.com>
2025-08-14 14:57:43 +0800

    Update client command

--- benchmarks/scripts/dense/client.sh
+++ benchmarks/scripts/dense/client.sh
@@ -1,12 +1,14 @@
-# bash client.sh /home/zejchen/models/amd/Llama-3.1-8B-Instruct-FP8-KV 1000 1024 1024
+# bash client.sh /data/models/amd/Llama-3.1-8B-Instruct-FP8-KV 1000 1024 1024
 
 MODEL=$1
 CONCUR=$2
 IN=$3
 OUT=$4
-NUM=$CONCUR * 10
+NUM=$(expr $CONCUR \* 10)
 
 result_filename=CON${CONCUR}_IN${IN}_OUT${OUT}.json
+result_log=CON${CONCUR}_IN${IN}_OUT${OUT}.log
+
 mkdir bench_results
 python3 ../../benchmark_serving.py \
     --backend vllm \
@@ -18,6 +20,9 @@ python3 ../../benchmark_serving.py \
     --max-concurrency ${CONCUR} \
     --random-input-len ${IN} \
     --random-output-len ${OUT} \
+    --save-result \
     --result-dir ./bench_results/ \
     --result-filename ${result_filename} \
-    --percentile-metrics "ttft,tpot,itl,e2el"
+    --percentile-metrics "ttft,tpot,itl,e2el" \
+    2>&1 | tee ./bench_results/${result_log}
+
